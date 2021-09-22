9e1ed822ce865906686908e05145d3ff0179613d
pattanawadee88 <winyaratp@wit.edu>
2021-09-22 14:29:14 -0400

    edit local date

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,89 @@
 import React from 'react'
 
 function Header(){
-    const mydate = new Date();
-    const todayDate = mydate.toDateString();
-    const localTime = mydate.toLocaleTimeString();
+    const todaydate = new Date();
+    let day;
+    let month;
+    switch(todaydate.getDay()){
+        case 0: day= "Sunday";
+                break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+       
+            break;
+        case 3:
+            day = "Wednesday";
+          
+            break;
+        case 4:
+            day = "Thursday";
+        
+            break;
+        case 5:
+            day = "Friday";
+
+            break;
+        case 6:
+            day = "Saturday";
     
+            break;
+        default: 
+            day = "Hello!";
+    }
+    switch(todaydate.getMonth()){
+        case 0: month = "January";
+                break;
+        case 1: month = "February";
+                break;
+                case 2:
+            
+                    month = "March";
+                    break;
+                case 3:
+               
+                    month = "April";
+                    break;
+                case 4:
+            
+                    month = "May";
+                    break;
+                case 5:
+         
+                    month = "June";
+                    break;
+                case 6:
+    
+                    month = "July";
+                    break;
+                case 7:
+                    month = "August";
+                    break;
+                case 8:
+                    month = "September";
+                    break;
+                case 9:
+                    month = "October";
+                    break;
+                case 10: 
+                    month = "November";
+                    break;
+                case 11: 
+                    month = "December";
+                    break;
+                default: 
+                    day = "Happy Month";
+    }
+    const localTime = todaydate.toLocaleTimeString();
+   
     return(
-        <header>
+        
+        <header id="header">
             <time>{localTime}</time>
             <h1 id="logo">Super News</h1>
-            <date>{todayDate}</date>
+            <div id="date"> {day},{month} {todaydate.getDate()}, {todaydate.getFullYear()}</div>
         </header>
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ function Nav(){
     return(
         <nav role="navigation">
             <ul>
-                <li><a href="#">Home</a></li>
+                <li><a href="#header">Home</a></li>
                 {/* <li><a href="#">Covid-19</a></li> */}
             </ul>
         </nav>
